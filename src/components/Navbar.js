eef803d517ea8a129768cee3bf43fbf3679f4201
Patrick <mussi_kat@hotmail.com>
2021-01-14 04:20:28 +0100

    Video is now showing in home page
    Getting the wrong button for sign up and logo has to be moved

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,4 +1,4 @@
-import React,{useState} from 'react';
+import React,{useState, useEffect} from 'react';
 import {Link} from "react-router-dom";
 import { Button } from './Button';
 import './Navbar.css';
@@ -17,13 +17,17 @@ function Navbar() {
             setButton(true);
         }
     }
-    
+    useEffect(() => {
+        showButton()
+    }, []);
+
     window.addEventListener('resize',showButton);
+
     return (
         <>
           <nav className ="navbar">
               <div className="navbar-container">
-              <Link to="/" className='navbar-logo'>
+              <Link to="/" className='navbar-logo' onClick={closeMobileMenu}>
                   Wrycken <i class='fas fa-dragon'></i>
               </Link> 
               <div className ='menu-icon' onClick={handleClick}>
